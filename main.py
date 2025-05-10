3dbe80fb18a4d2fb2a2e16e492fcff760b8c7058
pwups <aylapmacc@gmail.com>
2025-05-11 00:56:04 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 import discord
 from discord.ext import commands
-from keep_alive import keep_alive
+import keep_alive import keep_alive
 
 TOKEN = os.getenv("TOKEN")
 
@@ -17,7 +17,7 @@ class ClaimButton(discord.ui.View):
         self.requester = requester
         self.claimed = False
 
-    @discord.ui.button(label="Claim", style=discord.ButtonStyle.danger)
+    @discord.ui.button(label="claim", style=discord.ButtonStyle.danger)
     async def claim(self, interaction: discord.Interaction, button: discord.ui.Button):
         role = discord.utils.get(interaction.user.roles, id=ALLOWED_ROLE_ID)
         if role is None:
@@ -37,7 +37,7 @@ class ClaimButton(discord.ui.View):
 
 @bot.command()
 async def req(ctx, *, request: str):
-    message = f"_ _\n　　<@&1370091989714079845>\n　୨୧　{ctx.author.mention} requested **{request}**\n-# _ _　　　upldrs, click button to claim　:z__natsugroove:\n_ _"
+    message = f"_ _\n　　<@&1370091989714079845>\n　୨୧　{ctx.author.mention} requested **{request}**\n-# _ _　　　upldrs, click button to claim　<a:z__natsugroove:1353422820335554632>\n_ _"
     view = ClaimButton(requester=ctx.author)
     await ctx.send(content=message, view=view)
 
