6fb3cf9874cb905505efa8c82dbf40343a249a73
pwups <aylapmacc@gmail.com>
2025-05-11 00:42:51 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import os
 import discord
 from discord.ext import commands
-from keep_alive import keep_alive  # Keeps the bot alive on Render
 
 TOKEN = os.getenv("TOKEN")
 
@@ -20,24 +19,24 @@ class ClaimButton(discord.ui.View):
     @discord.ui.button(label="Claim", style=discord.ButtonStyle.danger)
     async def claim(self, interaction: discord.Interaction, button: discord.ui.Button):
         role = discord.utils.get(interaction.user.roles, id=ALLOWED_ROLE_ID)
-    if role is None:
-        await interaction.response.send_message("You don't have permission to claim this request.", ephemeral=True)
-        return
+        if role is None:
+            await interaction.response.send_message("You don't have permission to claim this request.", ephemeral=True)
+            return
 
-    if not self.claimed:
-        self.claimed = True
-        button.disabled = True
-        await interaction.message.edit(view=self)
-        await interaction.message.reply(
-            f"꒰˘ᵌ˘꒱♡  .  .  {interaction.user.mention} has claimed this request *!*"
-        )
-        await interaction.response.defer()
-    else:
-        await interaction.response.send_message("This request has already been claimed.", ephemeral=True)
+        if not self.claimed:
+            self.claimed = True
+            button.disabled = True
+            await interaction.message.edit(view=self)
+            await interaction.message.reply(
+                f"꒰˘ᵌ˘꒱♡  .  .  {interaction.user.mention} has claimed this request *!*"
+            )
+            await interaction.response.defer()
+        else:
+            await interaction.response.send_message("This request has already been claimed.", ephemeral=True)
 
 @bot.command()
 async def req(ctx, *, request: str):
-    message = f"_ _\n　　<@&1370091989714079845>\n　୨୧　{ctx.author.mention} requested **{request}**\n-# _ _　　　upldrs, click button to claim　<a:z__natsugroove:1353422820335554632>\n_ _"
+    message = f"_ _\n　　<@&1370091989714079845>\n　୨୧　{ctx.author.mention} requested **{request}**\n-# _ _　　　upldrs, click button to claim　:z__natsugroove:\n_ _"
     view = ClaimButton(requester=ctx.author)
     await ctx.send(content=message, view=view)
 
@@ -47,5 +46,4 @@ async def on_ready():
     await bot.change_presence(status=discord.Status.dnd, activity=activity)
     print(f'Logged in as {bot.user.name}')
 
-keep_alive()  # Start web server to prevent Render from sleeping
 bot.run(TOKEN)
